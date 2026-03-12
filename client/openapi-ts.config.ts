6584cf3914f4c2f6c8cca2eb21beba5ba0b20f7c
oskar-dragon <32719913+oskar-dragon@users.noreply.github.com>
2026-03-12 11:18:41 +0000

    .

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
   input: "openapi.json",
-  output: "src",
+  output: "src/generated",
   plugins: ["@hey-api/client-fetch", "@hey-api/typescript", "@hey-api/sdk"],
 });
